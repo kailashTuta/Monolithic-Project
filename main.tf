4a9c7dd80fcedec856962264a54611b12db7f698
Kailash Tuta <62685303+kailashTuta@users.noreply.github.com>
2026-06-12 15:20:01 +0530

    Update ELB subnets in main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
+     subnets = ["subnet-0a506b405f33533a8", "subnet-0f7efd8fa4c1a0702"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
