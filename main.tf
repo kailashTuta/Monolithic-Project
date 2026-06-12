e1d0b84f13fc5037f700f71efba2c87b92e84182
Kailash Tuta <62685303+kailashTuta@users.noreply.github.com>
2026-06-12 15:17:43 +0530

    Update AMI ID for web server launch template

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-02dfbd4ff395f2a1b"
+    image_id           = "ami-0152204c1a187337c"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
     key_name = "march-kp"
