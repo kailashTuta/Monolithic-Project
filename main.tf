4b1dce0968bddf685ede4c5669eed81c9003e7d7
Kailash Tuta <62685303+kailashTuta@users.noreply.github.com>
2026-06-28 22:33:15 +0530

    Chnaged instance port

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_launch_template" "web_server_as" {
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-0fd64d64a7f62bd31", "subnet-0652d279ab058b229"]
      listener {
-      instance_port     = 8000
+      instance_port     = 80
       instance_protocol = "http"
       lb_port           = 80
       lb_protocol       = "http"
